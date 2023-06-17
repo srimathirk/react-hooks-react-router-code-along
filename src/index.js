35f632e3600636255c8bfcd1c31f42cc68c872ac
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-06-17 14:09:50 -0400

    code along complete

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+/* Add NavLink to import */
+import { BrowserRouter} from "react-router-dom";
+import App from "./components/App";
 
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-}
+/* add the NavBar component to our App component */
 
-function App() {
-  return <Home />;
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+   <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
